bac597de57a5feb297f213258701347bf0f202fa
Ekin Inceleme <ekin.inceleme@ocado.com>
2020-12-07 17:37:30 +0000

    Remove first way of doing it

--- src/main/java/day7/Day7Node.java
+++ src/main/java/day7/Day7Node.java
@@ -8,28 +8,28 @@ import java.util.stream.Stream;
 
 public class Day7Node {
     public static long processStringStreamPart1(String keyString, Stream<String> lines) {
-        Map<String, ColouredBag> bagMap = lines.map(ColouredBag::buildFrom)
-                .collect(Collectors.toUnmodifiableMap(ColouredBag::getColour, Function.identity()));
-        Set<ColouredBagNode> bagNodeSet = bagMap.values()
-                .stream()
-                .map(bag -> ColouredBagNode.buildFrom(bag, bagMap))
-                .collect(Collectors.toSet());
+        Map<String, ColouredBagNode> bagNodeMap = getStringColouredBagNodeMap(lines);
 
-        return bagNodeSet.stream()
+        return bagNodeMap.values()
+                .stream()
                 .map(colouredBag -> colouredBag.containsBagColour(keyString))
                 .filter(aBoolean -> aBoolean)
                 .count();
     }
 
     public static long processStringStreamPart2(String keyString, Stream<String> lines) {
-        Map<String, ColouredBag> bagMap = lines.map(ColouredBag::buildFrom)
-                .collect(Collectors.toUnmodifiableMap(ColouredBag::getColour, Function.identity()));
-        Map<String, ColouredBagNode> bagNodeMap = bagMap.values()
-                .stream()
-                .map(bag -> ColouredBagNode.buildFrom(bag, bagMap))
-                .collect(Collectors.toUnmodifiableMap(ColouredBagNode::getColour, Function.identity()));
+        Map<String, ColouredBagNode> bagNodeMap = getStringColouredBagNodeMap(lines);
 
         ColouredBagNode keyBagNode = bagNodeMap.get(keyString);
         return keyBagNode.countBags();
+    }
+
+    private static Map<String, ColouredBagNode> getStringColouredBagNodeMap(Stream<String> lines) {
+        Map<String, ColouredBag> bagMap = lines.map(ColouredBag::buildFrom)
+                .collect(Collectors.toUnmodifiableMap(ColouredBag::getColour, Function.identity()));
+        return bagMap.values()
+                .stream()
+                .map(bag -> ColouredBagNode.buildFrom(bag, bagMap))
+                .collect(Collectors.toUnmodifiableMap(ColouredBagNode::getColour, Function.identity()));
     }
 }
